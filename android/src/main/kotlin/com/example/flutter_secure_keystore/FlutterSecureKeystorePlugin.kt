14e2cddca3abb9ff826036345c79d4b7628fbabb
Gabriel Mello <gabrielinaciodemello@gmail.com>
2023-08-30 14:48:53 -0300

    WIP

--- android/src/main/kotlin/com/example/flutter_secure_keystore/FlutterSecureKeystorePlugin.kt
+++ android/src/main/kotlin/com/example/flutter_secure_keystore/FlutterSecureKeystorePlugin.kt
@@ -1,5 +1,9 @@
 package com.example.flutter_secure_keystore
 
+import android.content.Context
+import android.content.DialogInterface
+import android.hardware.biometrics.BiometricPrompt
+import android.os.CancellationSignal
 import androidx.annotation.NonNull
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
@@ -9,21 +13,23 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
-import io.flutter.Log
-import java.security.SecureRandom
+import android.util.Base64
+import java.security.KeyStore
 import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
-import javax.crypto.spec.GCMParameterSpec
 import javax.crypto.spec.IvParameterSpec
 
 /** FlutterSecureKeystorePlugin */
 class FlutterSecureKeystorePlugin: FlutterPlugin, MethodCallHandler {
+
   private lateinit var channel : MethodChannel
+  private lateinit var context: Context
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_secure_keystore")
     channel.setMethodCallHandler(this)
+    context = flutterPluginBinding.applicationContext
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
@@ -51,67 +57,115 @@ class FlutterSecureKeystorePlugin: FlutterPlugin, MethodCallHandler {
           result.error("INVALID_INPUT", "Alias or data was not provided.", null)
           return;
         }
-        try {
-          val encrypted = encrypt(alias, data)
-          result.success(encrypted)
-        } catch (e: Exception) {
-          result.error("ENCRYPTION_FAILED", e.message, e)
-        }
+        authenticateUser(
+          this.context,
+          onSuccess = {
+            try {
+              val encrypted = encrypt(alias, data)
+              result.success(encrypted)
+            } catch (e: Exception) {
+              result.error("ENCRYPTION_FAILED", e.message, e)
+            }
+          },
+          onFailure = {
+            result.error("ENCRYPTION_FAILED", "User is not authenticated", null)
+          }
+        )
       }
       "decrypt" -> {
         val alias = call.argument<String>("alias")
         val encryptedData = call.argument<String>("data")
-        if (alias != null && encryptedData != null) {
-          try {
-            val decrypted = decrypt(alias, encryptedData)
-            result.success(decrypted)
-          } catch (e: Exception) {
-            result.error("DECRYPTION_FAILED", e.message, null)
-          }
-        } else {
+        if(alias == null || encryptedData == null){
           result.error("INVALID_INPUT", "Alias or encrypted data was not provided.", null)
+          return
         }
+        authenticateUser(
+          this.context,
+          onSuccess = {
+            try {
+              val decrypted = decrypt(alias, encryptedData)
+              result.success(decrypted)
+            } catch (e: Exception) {
+              result.error("DECRYPTION_FAILED", e.message, e)
+            }
+          },
+          onFailure = {
+            result.error("DECRYPTION_FAILED", "User is not authenticated", null)
+          }
+        )
       }
       else -> result.notImplemented()
     }
   }
 
   private fun createKey(alias: String) {
-      val keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore")
+      val keyGenerator = KeyGenerator.getInstance(
+              KeyProperties.KEY_ALGORITHM_AES,
+              "AndroidKeyStore"
+      )
       val keyGenParameterSpec = KeyGenParameterSpec
               .Builder(alias,KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
               .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
               .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
               .setUserAuthenticationRequired(true)
+              .setUserAuthenticationValidityDurationSeconds(30)
               .build()
 
       keyGenerator.init(keyGenParameterSpec)
       val secretKey = keyGenerator.generateKey()
   }
 
-  private fun encrypt(alias: String, data: String): String {
-    val cipher = Cipher.getInstance("${KeyProperties.KEY_ALGORITHM_AES}/${KeyProperties.BLOCK_MODE_CBC}/${KeyProperties.ENCRYPTION_PADDING_PKCS7}")
-    val secretKey: SecretKey = retrieveKey(alias)
-    cipher.init(Cipher.ENCRYPT_MODE, secretKey)
-    val encryptedBytes = cipher.doFinal(data.toByteArray(Charsets.UTF_8))
-    val ivBytes = cipher.iv
-    val combined = ivBytes + encryptedBytes
-    return android.util.Base64.encodeToString(combined, android.util.Base64.DEFAULT)
-  }
-
-
-  private fun decrypt(alias: String, encryptedData: String): String {
-      val cipher = Cipher.getInstance("${KeyProperties.KEY_ALGORITHM_AES}/${KeyProperties.BLOCK_MODE_CBC}/${KeyProperties.ENCRYPTION_PADDING_PKCS7}")
-      val secretKey: SecretKey = retrieveKey(alias)
-      cipher.init(Cipher.DECRYPT_MODE, secretKey, GCMParameterSpec(128, encryptedData.toByteArray(Charsets.UTF_8)))
-      val decryptedBytes = cipher.doFinal(android.util.Base64.decode(encryptedData, android.util.Base64.DEFAULT))
-      return String(decryptedBytes, Charsets.UTF_8)
-  }
-
   private fun retrieveKey(alias: String): SecretKey {
-    val keyStore = java.security.KeyStore.getInstance("AndroidKeyStore")
+    val keyStore = KeyStore.getInstance("AndroidKeyStore")
     keyStore.load(null)
     return keyStore.getKey(alias, null) as SecretKey
+  }
+
+  private fun encrypt(alias: String, data: String): String {
+    val secretKey = retrieveKey(alias)
+    val cipher = Cipher.getInstance("${KeyProperties.KEY_ALGORITHM_AES}/${KeyProperties.BLOCK_MODE_CBC}/${KeyProperties.ENCRYPTION_PADDING_PKCS7}")
+    cipher.init(Cipher.ENCRYPT_MODE, secretKey)
+    val encrypted = cipher.doFinal(data.toByteArray())
+    val combined = cipher.iv + encrypted
+    return Base64.encodeToString(combined, Base64.DEFAULT)
+  }
+
+  private fun decrypt(alias: String, encryptedData: String): String {
+    val secretKey = retrieveKey(alias)
+    val combined = Base64.decode(encryptedData, Base64.DEFAULT)
+    val iv = combined.sliceArray(0 until 16) // 16 bytes for IV
+    val encryptedData = combined.sliceArray(16 until combined.size)
+    val cipher = Cipher.getInstance("${KeyProperties.KEY_ALGORITHM_AES}/${KeyProperties.BLOCK_MODE_CBC}/${KeyProperties.ENCRYPTION_PADDING_PKCS7}")
+    cipher.init(Cipher.DECRYPT_MODE, secretKey, IvParameterSpec(iv))
+    return String(cipher.doFinal(encryptedData))
+  }
+
+  fun authenticateUser(context: Context, onSuccess: () -> Unit, onFailure: () -> Unit) {
+    val biometricPrompt = BiometricPrompt.Builder(context)
+            .setTitle("Authentication required")
+            .setSubtitle("Please auth to proceed.")
+            .setNegativeButton("Cancel", context.mainExecutor, DialogInterface.OnClickListener { _, _ ->
+              onFailure()
+            })
+            .build()
+    val cancellationSignal = CancellationSignal()
+    cancellationSignal.setOnCancelListener {
+      onFailure()
+    }
+    biometricPrompt.authenticate(cancellationSignal, context.mainExecutor, object : BiometricPrompt.AuthenticationCallback() {
+      override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult?) {
+        super.onAuthenticationSucceeded(result)
+        onSuccess()
+      }
+      override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
+        super.onAuthenticationError(errorCode, errString)
+        onFailure()
+      }
+      override fun onAuthenticationFailed() {
+        super.onAuthenticationFailed()
+        onFailure()
+      }
+    })
   }
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
